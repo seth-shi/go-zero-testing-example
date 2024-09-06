1fcf96a191b4f81a2598c4b6626024ce7e0ee902
seth-shi <1033404553@qq.com>
2024-09-06 16:21:58 +0800

    add: config file test

--- app/post/rpc/post_test.go
+++ app/post/rpc/post_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/seth-shi/go-zero-testing-example/app/post/rpc/internal/faker"
 	"github.com/seth-shi/go-zero-testing-example/app/post/rpc/internal/svc"
 	"github.com/seth-shi/go-zero-testing-example/app/post/rpc/post"
+	"github.com/seth-shi/go-zero-testing-example/pkg"
 	"github.com/stretchr/testify/require"
 	"github.com/zeromicro/go-zero/zrpc"
 )
@@ -20,6 +21,18 @@ func TestMain(m *testing.M) {
 	svcCtxGet = func(c config.Config) *svc.ServiceContext {
 		return faker.GetValue().SvcCtx
 	}
+
+	data := `Name: post.rpc
+ListenOn: 0.0.0.0:8080
+DataSource: 127.0.0.1:3306?charset=utf8mb4&parseTime=true&loc=Local
+IdRpc:
+  Target: 0.0.0.0:8081
+RedisConf:
+  Host: 127.0.0.1:6379
+`
+	remove, tmp := lo.Must2(pkg.CreateTempFile(".yaml", data))
+	defer remove()
+	configFile = tmp
 
 	go main()
 
